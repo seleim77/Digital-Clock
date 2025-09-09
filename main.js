ff37cb282281e80475552a31aa5d7721ef4607e1
seleim77 <abdelrahmanselim12345@gmail.com>
2025-09-09 06:05:00 +0300

    finished

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ function updateTime() {
         mode.innerText = "AM";
     }
 
-    h = h > 10 ? "0" + h : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
     hours.innerText = h;
     minutes.innerText = m;
