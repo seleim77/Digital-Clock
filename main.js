4e8d48dfb2697c84fa9d5c10263f0dab6efd53ba
seleim77 <abdelrahmanselim12345@gmail.com>
2025-09-09 05:20:05 +0300

    update

---
+++ main.js
@@ -0,0 +1,30 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const mode = document.getElementById("mode");
+
+updateTime();
+
+function updateTime() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    if (h > 12) {
+        h = h - 12;
+        mode.innerText = "PM";
+    } else {
+        h = h;
+        mode.innerText = "AM";
+    }
+
+    h = h > 10 ? "0" + h : h;
+
+    hours.innerText = h;
+    minutes.innerText = m;
+    seconds.innerText = s;
+
+    setTimeout(() => {
+        updateTime();
+    }, 1000);
+}
